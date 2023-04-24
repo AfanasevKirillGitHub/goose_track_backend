da8f309acec4f5cf6a7bb81cd82738c032236d7e
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-04-24 18:26:13 +0300

    fix translate2

--- helpers/translateBody.js
+++ helpers/translateBody.js
@@ -2,29 +2,29 @@ const translateField = require("./translateField");
 
 // Функція перекладу БОДІ
 const translateBody = async (sourceLang, targetLang, body) => {
-  const newBody = Object.keys(body).reduce(async (acc, key) => {
+  const newBody = await Object.keys(body).reduce(async (acc, lang) => {
     // База рекурсії
-    if (typeof body[key] !== "object") {
-      if (key === sourceLang) {
+    if (typeof body[lang] !== "object") {
+      if (lang === sourceLang) {
         // нормалізуємо таргетну мову для бази щоб в базі було UA
         const normalizedTargetLang = targetLang === "ua" ? "uk" : "en";
         // нормалізуємо ключ вхідної мови для функції перекладу на UK
-        const normalizedFromLangKey = key === "ua" ? "uk" : "en";
+        const normalizedFromLangKey = lang === "ua" ? "uk" : "en";
 
         body[targetLang] = await translateField({
-          text: body[sourceLang],
+          text: await body[sourceLang],
           from: normalizedFromLangKey,
           to: normalizedTargetLang,
         });
         return await body;
       }
 
-      return { ...(await acc), [key]: body[key] };
+      return { ...(await acc), [lang]: await body[lang] };
     } else {
       // Наступний крок рекурсії
       return {
         ...(await acc),
-        [key]: await translateBody(sourceLang, targetLang, body[key]),
+        [lang]: await translateBody(sourceLang, targetLang, await body[lang]),
       };
     }
   }, {});
