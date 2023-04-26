b7bc79077b9a07c215347e55342473072760fd8d
SPoluliakh <S_Poluliakh@ukr.net>
2023-04-26 15:53:38 +0300

    fix

--- controllers/reviews/getAllReviews.js
+++ controllers/reviews/getAllReviews.js
@@ -1,15 +1,13 @@
-const { Rewiew } = require("../../models/review");
+const { Review } = require("../../models/review");
 
 const getAllRewiews = async (req, res) => {
-  
-    const rewiew = await Rewiew.find( )
-  
-         res.status(200).json({
-      message: "Successfully",
-      rewiew,
-      total: rewiew.length,
-    });
-  };
-  
-  module.exports = getAllRewiews;
-  
+  const rewiew = await Review.find();
+
+  res.status(200).json({
+    message: "Successfully",
+    rewiew,
+    total: rewiew.length,
+  });
+};
+
+module.exports = getAllRewiews;
