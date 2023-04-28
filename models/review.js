585aeae3872ce13453635aa1f8cef6cfa76abfed
GeorgiyDer <106014816+GeorgiyDer@users.noreply.github.com>
2023-04-28 18:25:02 +0300

    del require Img Url

--- models/review.js
+++ models/review.js
@@ -25,7 +25,7 @@ const reviewsSchema = new Schema(
       },
       imgUrl: {
         type: String,
-        required: true,
+        // required: true,
         default: "",
         description: "Name",      
       },
